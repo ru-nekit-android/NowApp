d5525d4a1ff65270143e8041ddcc754ec1916753
nekit <ru.nekit.android@gmail.com>
2015-04-18 02:43:36 +1000

    version 1.1.5

--- app/src/main/java/ru/nekit/android/nowapp/view/EventDetailFragment.java
+++ app/src/main/java/ru/nekit/android/nowapp/view/EventDetailFragment.java
@@ -292,7 +292,7 @@ public class EventDetailFragment extends Fragment implements View.OnClickListene
 
         mMapViewContainer = (RelativeLayout) view.findViewById(R.id.map_view_container);
 
-        ((ScrollView) view.findViewById(R.id.scroll_view)).fullScroll(ScrollView.FOCUS_DOWN);
+        view.findViewById(R.id.scroll_view).scrollTo(0, 0);
 
         return view;
     }
