7adb17ad4a0ceac0e3d08f6dd3dec15bc58351a2
nekit <ru.nekit.android@gmail.com>
2015-05-11 23:52:43 +1000

    EventToCalendarLoader: get event id from args

--- app/src/main/java/ru/nekit/android/nowapp/model/EventToCalendarLoader.java
+++ app/src/main/java/ru/nekit/android/nowapp/model/EventToCalendarLoader.java
@@ -14,60 +14,65 @@ import java.util.Calendar;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
-import ru.nekit.android.nowapp.model.db.vo.EventToCalendarLink;
+import ru.nekit.android.nowapp.NowApplication;
+import ru.nekit.android.nowapp.model.vo.EventToCalendarLink;
 
 /**
  * Created by chuvac on 30.04.15.
  */
 public class EventToCalendarLoader extends AsyncTaskLoader<EventToCalendarLink> {
 
-    public static final String EVENT_ITEM_KEY = "event_item_id_key";
+    public static final String EVENT_ITEM_ID_KEY = "event_item_id_key";
     public static final String METHOD_KEY = "method_key";
     public static final int CHECK = 1;
     public static final int ADD = 2;
 
     private final int mMethod;
-    private final EventItem mEventItem;
+    private final int mEventItemId;
 
     public EventToCalendarLoader(Context context, Bundle args) {
         super(context);
         mMethod = args.getInt(METHOD_KEY);
-        mEventItem = args.getParcelable(EVENT_ITEM_KEY);
+        mEventItemId = args.getInt(EVENT_ITEM_ID_KEY);
     }
 
     @Override
     public EventToCalendarLink loadInBackground() {
         EventToCalendarLink result = null;
         long calendarEventID;
-        Context context = getContext();
-        EventItemsModel model = EventItemsModel.getInstance(context);
-        model.getEventToCalendarLinker().openForWrite();
+        Context context = getContext().getApplicationContext();
+        EventItemsModel model = NowApplication.getEventModel();
+        model.openEventToCalendarLinker(true);
         ContentResolver contentResolver = context.getContentResolver();
         if (mMethod == CHECK) {
-            result = model.getEventToCalendarLink(mEventItem);
+            result = model.getEventToCalendarLinkByEventId(mEventItemId);
             if (result != null) {
                 calendarEventID = result.getCalendarEventID();
                 Cursor cursor = contentResolver.query(ContentUris.withAppendedId(CalendarContract.Events.CONTENT_URI, calendarEventID), null, null, null, null);
                 if (!cursor.moveToFirst()) {
                     result = null;
-                    model.removeEventToCalendarLink(mEventItem);
+                    model.removeEventToCalendarLinkByEventId(mEventItemId);
                 }
                 cursor.close();
             }
         } else if (mMethod == ADD) {
+            EventItem eventItem = model.getEventItemByID(mEventItemId);
             TimeZone timeZone = Calendar.getInstance().getTimeZone();
             ContentValues values = new ContentValues();
-            values.put(CalendarContract.Events.DTSTART, TimeUnit.SECONDS.toMillis(EventItemsModel.getEventStartTimeInSeconds(mEventItem)));
-            values.put(CalendarContract.Events.DTEND, TimeUnit.SECONDS.toMillis(EventItemsModel.getEventEndTimeInSeconds(mEventItem)));
-            values.put(CalendarContract.Events.TITLE, mEventItem.name);
-            values.put(CalendarContract.Events.EVENT_LOCATION, mEventItem.placeName);
-            values.put(CalendarContract.Events.DESCRIPTION, mEventItem.eventDescription);
-            values.put(CalendarContract.Events.EVENT_COLOR, EventItemsModel.getCategoryColor(mEventItem.category));
+            values.put(CalendarContract.Events.DTSTART, TimeUnit.SECONDS.toMillis(EventItemsModel.getEventStartTimeInSeconds(eventItem)));
+            values.put(CalendarContract.Events.DTEND, TimeUnit.SECONDS.toMillis(EventItemsModel.getEventEndTimeInSeconds(eventItem)));
+            values.put(CalendarContract.Events.TITLE, eventItem.name);
+            values.put(CalendarContract.Events.EVENT_LOCATION, eventItem.placeName);
+            if (!"".equals(eventItem.email)) {
+                values.put(CalendarContract.Events.ORGANIZER, eventItem.email);
+            }
+            values.put(CalendarContract.Events.DESCRIPTION, eventItem.eventDescription);
+            values.put(CalendarContract.Events.EVENT_COLOR, EventItemsModel.getCategoryColor(eventItem.category));
             values.put(CalendarContract.Events.EVENT_TIMEZONE, timeZone.getID());
             values.put(CalendarContract.Events.CALENDAR_ID, 1);
             Uri insertToCalendarURI = contentResolver.insert(CalendarContract.Events.CONTENT_URI, values);
             calendarEventID = Long.parseLong(insertToCalendarURI.getLastPathSegment());
-            result = model.addEventToCalendarLink(mEventItem, calendarEventID);
+            result = model.addEventToCalendarLink(eventItem, calendarEventID);
         }
         return result;
     }
